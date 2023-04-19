75a5011152e33e708bf9fdb1219222e4d4115813
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-04-19 15:26:48 +0100

    Refactor current code

--- app.js
+++ app.js
@@ -1,78 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
-const Event = require('./models/event');
+const graphiqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
 
 app.use(bodyParser.json());
 
-//add extra fields later after data and types setup and testing
-
 app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            date: String!
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            date: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-        }
-
-        schema {
-            query: RootQuery 
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find()
-            .then(events => {
-                return events.map(event => {
-                    return {...event._doc, _id: event.id};
-                })
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            })
-        },
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                date: new Date(args.eventInput.date)
-            });
-            return event
-            .save()
-            .then(result => {
-                console.log(result);
-                return {...result._doc, _id: event.id};
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            });
-            
-        }
-    },
+    schema: graphiqlSchema,
+    rootValue: graphqlResolvers,
     graphiql: true
 }));
 
